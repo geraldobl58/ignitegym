bea5c18912c23fc98f2a805f0dc1bdf31c295057
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-25 14:19:25 -0300

    feat: Add input component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2,10 +2,11 @@ import { VStack, Image, Text, Center, Heading } from 'native-base'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
+import { Input } from '@components/Input'
 
 export function SignIn() {
   return (
-    <VStack flex={1} bg="gray.700">
+    <VStack flex={1} bg="gray.700" px={10}>
       <Image 
         source={BackgroundImg}
         alt="Pessoas treinando"
@@ -25,6 +26,18 @@ export function SignIn() {
         <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
           Acesse sua conta
         </Heading>
+
+        <Input 
+          placeholder="E-mail"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+
+        
+        <Input 
+          placeholder="Senha"
+          secureTextEntry 
+        />
       </Center>
     </VStack>
   )
