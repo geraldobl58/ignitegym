121e673c14d4ee74e7053fa8c8964c629c2a82fa
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-26 15:48:52 -0300

    feat: Adds group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,11 +1,45 @@
-import { Center, Text, VStack } from "native-base";
+import { useState } from "react";
+import { FlatList, Heading, HStack, Text, VStack } from "native-base";
 
 import { Header } from "@components/Header";
+import { Group } from "@components/Group";
 
 export function Home() {
+  const [groups, setGroups] = useState(['Costas', 'Bíceps', 'Tríceps', 'ombro'])
+  const [groupSelected, setGroupSelected] = useState('costa')
+
   return (
     <VStack flex={1}>
       <Header />
+
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            name={item}
+            isActive={groupSelected === item}
+            onPress={() => setGroupSelected(item)} 
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercícios
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   )
 }
