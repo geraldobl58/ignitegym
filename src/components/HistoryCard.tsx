aea1eb07f554816f41076e59aabbd5ac1cd6ff92
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-27 11:25:02 -0300

    feat: Add story card component

---
+++ src/components/HistoryCard.tsx
@@ -0,0 +1,41 @@
+import { Heading, HStack, Text, VStack } from "native-base";
+
+export function HistoryCard() {
+  return (
+    <HStack
+      w="full"
+      px={5}
+      py={4}
+      mb={3}
+      bg="gray.600"
+      rounded="md"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <VStack mr={5}>
+        <Heading 
+          color="white" 
+          fontSize="md" 
+          textTransform="capitalize"
+        >
+          Nado borboleta
+        </Heading>
+
+        <Text
+          color="gray.100"
+          fontSize="lg"
+          numberOfLines={1}
+        >
+          5 chegada de 50 metros
+        </Text>
+      </VStack>
+
+      <Text
+        color="gray.300"
+        fontSize="md"
+      >
+        07:00
+      </Text>
+    </HStack>
+  )
+}
--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,11 +1,14 @@
 import { VStack } from "native-base";
 
 import { Heading } from "@components/Heading";
+import { HistoryCard } from "@components/HistoryCard";
 
 export function History() {
   return (
     <VStack>
       <Heading title="Histórico de Exercícios" />
+
+      <HistoryCard />
     </VStack>
   )
 }
