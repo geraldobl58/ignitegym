93bb160bb4cac095a2c49f37756538116c630a50
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-28 19:42:10 -0300

    feat: Add form and input validation

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,18 +1,52 @@
 import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
+import { useForm, Controller } from 'react-hook-form'
+
+import { yupResolver } from '@hookform/resolvers/yup'
+
+import * as yup from 'yup'
+
 import { Input } from '@components/Input'
 import { Button } from '@components/Buttton'
 
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 
+type FormDataProps = {
+  name: string
+  email: string
+  password: string
+  password_confirm: string
+}
+
+const signUpSchema = yup.object({
+  name: yup.string()
+    .required('Informe o nome.'),
+  email: yup.string()
+    .required('Informe o e-mail.')
+    .email('E-mail inválido.'),
+  password: yup.string().
+    required('Informe a senha.')
+    .min(6, 'A senha deve ter pelo menos 6 caracteres.'),
+  password_confirm: yup.string()
+    .required('Confirme a senha.')
+    .oneOf([yup.ref('password'), null], 'As senha precisam ser iguais.')
+})
+
 export function SignUp() {
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  })
 
   const navigation = useNavigation()
 
   function handleGoBack() {
     navigation.goBack()
+  }
+
+  function handleSignUp(data: FormDataProps) {
+    console.log(data)
   }
 
   return (
@@ -30,7 +64,7 @@ export function SignUp() {
         />
 
         <Center my={24}>
-            <LogoSvg />
+          <LogoSvg />
 
           <Text color="gray.100" fontSize="sm">
             Treine sua mente e o seu corpo
@@ -42,23 +76,67 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input 
-            placeholder="Nome"
+          <Controller 
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
 
-          <Input 
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller 
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
           />
 
-          <Input 
-            placeholder="Senha"
-            secureTextEntry 
+          <Controller 
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Controller 
+            control={control}
+            name="password_confirm"
+            render={({ field: { onChange, value } }) => (
+              <Input 
+                placeholder="Confirmar senha"
+                secureTextEntry 
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.password_confirm?.message}
+              />
+            )}
           />
 
           <Button 
             title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
           />
 
         </Center>
