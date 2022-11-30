2149500ec1cffbee1d8af7560034a581a3667703
Geraldo Luiz <geraldobl58@gmail.com>
2022-11-30 18:56:45 -0300

    feat: Add user registration with api

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -45,8 +45,15 @@ export function SignUp() {
     navigation.goBack()
   }
 
-  function handleSignUp(data: FormDataProps) {
-    console.log(data)
+  function handleSignUp({ name, email, password }: FormDataProps) {
+    fetch('http://192.168.15.12:3333/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name, email, password })
+    })
   }
 
   return (
