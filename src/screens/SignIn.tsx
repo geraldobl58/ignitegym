b977b4a78665737ef53c6776f011e30807dfae22
Geraldo Luiz <geraldobl58@gmail.com>
2022-12-02 15:37:42 -0300

    feat: Add validation to sign in form

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,6 +1,8 @@
 import { useNavigation } from '@react-navigation/native'
 import { VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
 
+import { Controller, useForm } from 'react-hook-form'
+
 import { AuthNavigatorRoutesProps } from '@routes/auth.routes'
 
 import { Input } from '@components/Input'
@@ -9,12 +11,23 @@ import { Button } from '@components/Buttton'
 import BackgroundImg from '@assets/background.png'
 import LogoSvg from '@assets/logo.svg'
 
+type FormData = {
+  email: string
+  password: string
+}
+
 export function SignIn() {
   
   const navigation = useNavigation<AuthNavigatorRoutesProps>()
 
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>()
+
   function handleNewAccount() {
     navigation.navigate('signUp')
+  }
+
+  function handleSignIn({ email, password }: FormData) {
+    console.log(email, password )
   }
 
   return (
@@ -44,20 +57,38 @@ export function SignIn() {
             Acesse sua conta
           </Heading>
 
-          <Input 
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
+          <Controller 
+            control={control}
+            name="email"
+            rules={{ required: 'Informe seu e-mail' }}
+            render={({ field: { onChange } }) => (
+              <Input 
+                placeholder="E-mail"
+                keyboardType="email-address"
+                onChangeText={onChange}
+                errorMessage={errors.email?.message}
+                autoCapitalize="none"
+              />
+            )}
           />
 
-          
-          <Input 
-            placeholder="Senha"
-            secureTextEntry 
+          <Controller 
+            control={control}
+            name="password"
+            rules={{ required: 'Informe sua senha' }}
+            render={({ field: { onChange } }) => (
+              <Input 
+                secureTextEntry
+                placeholder="Senha"
+                onChangeText={onChange}
+                errorMessage={errors.password?.message}
+              />
+            )}
           />
 
           <Button 
             title="Acessar"
+            onPress={handleSubmit(handleSignIn)}
           />
 
         </Center>
