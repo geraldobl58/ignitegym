790e6ef76ad09806f29a8acd78b497a753eb6428
Geraldo Luiz <geraldobl58@gmail.com>
2022-12-06 19:17:05 -0300

    feat: Displaying Loading while loading user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,14 +5,19 @@ import { userAuth } from "@hooks/useAuth";
 
 import { AuthRoutes } from "./auth.routes";
 import { AppRoutes } from "./app.routes";
+import { Loading } from "@components/Loading";
 
 export function Routes() {
   const { colors } = useTheme()
 
-  const { user } = userAuth()
+  const { user, isLoadingUserStorageData } = userAuth()
 
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="gray.700">
