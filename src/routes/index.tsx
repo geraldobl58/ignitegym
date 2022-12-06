e793f8083fd2b42dd16c3ce154e74a40378906f5
Geraldo Luiz <geraldobl58@gmail.com>
2022-12-06 18:56:47 -0300

    feat: Redirect logged in user

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -11,15 +11,13 @@ export function Routes() {
 
   const { user } = userAuth()
 
-  console.log(user)
-
   const theme = DefaultTheme
   theme.colors.background = colors.gray[700]
 
   return (
     <Box flex={1} bg="gray.700">
       <NavigationContainer theme={theme}>
-        <AuthRoutes />
+        {user.id ? <AppRoutes /> : <AuthRoutes />}
       </NavigationContainer>
     </Box>
   )
