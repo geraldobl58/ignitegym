fdbdd5ba46538941ee7fe42989a4ae65b0cbd0e4
Geraldo Luiz <geraldobl58@gmail.com>
2022-12-12 20:03:51 -0300

    feat: Validating the password

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -37,7 +37,19 @@ type FormDataProps = {
 }
 
 const profileSchema = yup.object({
-  name: yup.string().required('Informe o nome.')
+  name: yup
+    .string()
+    .required('Informe o nome.'),
+  password: yup
+    .string()
+    .min(6, 'A senha deve ter pelo menos 6 dígitos.')
+    .nullable()
+    .transform((value) => !!value ? value : null),
+  confirm_password: yup
+    .string()
+    .nullable()
+    .transform((value) => !!value ? value : null)
+    .oneOf([yup.ref('password'), null], 'A confirmação de senha mão confere')
 })
 
 export function Profile() {
